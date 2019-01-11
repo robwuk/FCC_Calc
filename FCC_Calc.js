35596b28d82c5a9ae462ef3f3023103a889ec254
Robert Worth <robertworth@hotmail.com>
2019-01-11 15:35:10 +0000

    further work
    decimals working but not pasisng tests, need to look at consecutive operators

--- FCC_Calc.js
+++ FCC_Calc.js
@@ -1,8 +1,10 @@
   var lastAction;
   var allActions = new Array();
+  var equals = false;
+  var decimal = false;
 
   function clearData() {
-    lastAction = 0;
+    lastAction = "0";
     allActions.length = 0;
 
     updateTotal();
@@ -10,7 +12,7 @@
   }
 
   function updateTotal(){
-    document.getElementById("display__total").innerHTML = lastAction;
+    document.getElementById("display").innerHTML = lastAction;
   }
 
   function updateRunningTotal(){
@@ -24,7 +26,106 @@
       };
     }
 
-    document.getElementById("display__running").innerHTML = runningTotal
+    document.getElementById("display__running").innerHTML = runningTotal;
+  }
+
+  function isOperator(value){
+    if (value ==="+" || value ==="-" || value ==="/" || value ==="*") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function calculateSum(){
+    if (allActions.length >= 3) {
+      let value1 = allActions[0];
+      let operator = allActions[1];
+      let value2 = allActions[2];
+
+
+      value1 =  getResult(parseFloat(value1), operator, parseFloat(value2));
+
+      for (let i = 3; i< allActions.length; i+=2) {
+        if (allActions.length - i >= 2) {
+          operator = allActions[i];
+          value2 = allActions[i+1];
+          value1 = getResult(parseFloat(value1), operator, parseFloat(value2));
+        }
+      }
+
+      lastAction = value1.toString();
+      updateTotal();
+    }
+  }
+
+  function getResult(v1, op, v2){
+    switch (op) {
+      case "+":
+        return v1 + v2;
+        break;
+      case "*":
+        return v1 * v2;
+        break;
+      case "-":
+        return v1 - v2;
+        break;
+      case "/":
+        return v1 / v2;
+        break;
+    }
+  }
+
+  function buttonPress(value){
+
+    switch (value) {
+      case "+": case "-": case "/": case "*":
+        if (equals) {
+          allActions.length = 0;
+          allActions.push(lastAction);
+        }
+        allActions.push(value);
+        lastAction = value;
+        equals = false;
+        decimal = false;
+        break;
+      case "=":
+        equals = true;
+        calculateSum();
+        decimal = false;
+        break;
+      case ".":
+        equals = false;
+        if (!decimal){
+          if (isOperator(lastAction)) {
+            lastAction="0.";
+          } else {
+            allActions.pop();
+            lastAction = lastAction.concat(value);
+          }
+          allActions.push(lastAction);
+        }
+        decimal = true;
+        break;
+      default:
+        if (equals) {
+          lastAction = "";
+          allActions.length = 0;
+        } else if (isOperator(lastAction)) {
+          lastAction = "";
+        } else if (lastAction =="0"){
+          allActions.pop();
+          lastAction="";
+        } else {
+          allActions.pop();
+        }
+        lastAction = lastAction.concat(value);
+        allActions.push(lastAction);
+        equals = false;
+    }
+
+    updateRunningTotal();
+    updateTotal();
   }
 
 
